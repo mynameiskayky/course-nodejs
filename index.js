9efca2d52898271244bbe93984f7a3354e5e8f65
nynameiskayky <kaykyvieiraa@hotmail.com>
2023-05-02 19:27:30 -0300

    Automatic loading with nodemon

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express")
 const app = express()
 
+app.get("/", (req, res) => {
+  res.send("Bem vindo ao Formação NodeJs")
+})
+
+app.get("/blog", (req, res) => {
+  res.send("Bem vindo ao meu Blog!")
+})
+
+app.get("/canal/youtube", (req, res) => {
+  res.send("<h1>Olá! este é meu canal.</h1>")
+})
+
 app.listen(3333, (error) => {
   if(error) console.log("Ocorreu um erro.")
   else console.log("Servidor iniciado com sucesso!")
